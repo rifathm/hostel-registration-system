5b2205c99db54aedec600c8927cd27866f25af46
rifathm <rifadhmuhammadh96@gmail.com>
2021-05-01 19:02:44 +0530

    Refactoring

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const { check, validationResult } = require("express-validator");
+const path = require("path");
+const cors = require("cors");
 
 //routes
 const student = require("./routes/student");
-const signin = require("./routes/signin");
+const user = require("./routes/user");
 const hostel = require("./routes/hostel");
 const { populate } = require("./models/User");
 
 dotenv.config();
 const app = express();
+app.use(cors());
 app.use(express.json());
-app.use("/", express.static("./client/build/"));
 
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/";
 const PORT = process.env.PORT || 5000;
@@ -22,14 +24,24 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
+    useCreateIndex: true,
   })
   .then(() => {
-    app.listen(PORT, () => console.log(`Server listening at ${PORT}`));
+    console.log("MongoDb connected");
   })
   .catch((err) => {
     console.log(err);
   });
 
 app.use("/students", student);
-app.use("/user", signin);
-app.use("/hostel", hostel);
+app.use("/user", user);
+app.use("/hostels", hostel);
+
+if (process.env.ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client", "build")));
+  app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
+
+app.listen(PORT, () => console.log(`Server listening at ${PORT}`));
